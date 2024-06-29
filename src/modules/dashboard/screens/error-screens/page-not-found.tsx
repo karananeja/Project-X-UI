1bb2ed0e2ff152d88d59e2e90df4d6de0c388df1
karananeja <karananeja.r@gmail.com>
2024-06-29 16:13:55 +0530

    styling changes

--- src/modules/dashboard/screens/error-screens/page-not-found.tsx
+++ src/modules/dashboard/screens/error-screens/page-not-found.tsx
@@ -6,9 +6,7 @@ const PageNotFound = () => {
   return (
     <div className='w-full h-screen flex flex-col justify-center items-center'>
       <div className='w-3/5 h-[29rem] flex flex-col justify-center items-center'>
-        <span className='text-[1.625rem] mt-6 font-bold'>
-          We've lost this page
-        </span>
+        <span className='text-[1.625rem] font-bold'>We've lost this page</span>
         <p className='flex justify-center items-center mt-6 text-center'>
           Sorry, the page you are looking for doesn't exist.
         </p>
